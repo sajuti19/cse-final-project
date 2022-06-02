df881fd6e8d84e204c3682d8b4728291a8f83041
unknown <sajutidey19@gmail.com>
2022-06-03 00:49:11 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function show_hide() {
+    var a = document.getElementById("menu");
+    if (a.style.display === "block") { a.style.display = "none"; }
+    else { a.style.display = "block"; }
+}
+
